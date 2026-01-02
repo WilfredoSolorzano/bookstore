36a7985752a2d6ea995332d094129a10488ac4fa
WilfredoSolorzano <wsolorzano00@gmail.com>
2026-01-02 15:30:28 -0300

    terminando o bookstore

--- order/models/order.py
+++ order/models/order.py
@@ -4,10 +4,10 @@ from product.models import Product
 
 
 class Order(models.Model):
-    product: models.ManyToManyField[Product, models.Model] = models.ManyToManyField(Product, blank=False)
-    user: models.ForeignKey[User] = models.ForeignKey(User, on_delete=models.CASCADE)
-    quantity: models.IntegerField = models.IntegerField(default=1)
-    created_at: models.DateTimeField = models.DateTimeField(auto_now_add=True)
+    product = models.ManyToManyField(Product)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField(default=1)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order #{self.id}"
