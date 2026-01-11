57b81ddeabec7619eede69361a2238cfad35d296
WilfredoSolorzano <wsolorzano00@gmail.com>
2026-01-10 21:10:31 -0300

    Fix order/orderitem relationship

--- order/factories.py
+++ order/factories.py
@@ -1,11 +1,21 @@
 import factory
 from order.models import Order, OrderItem
 from product.factories import ProductFactory
+from django.contrib.auth.models import User
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    username = factory.Sequence(lambda n: f"user{n}")
 
 
 class OrderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Order
+
+    user = factory.SubFactory(UserFactory)
 
     @factory.post_generation
     def products(self, create, extracted, **kwargs):
