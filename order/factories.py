5ff1321097dc7ceba6762861597e464810998e4b
WilfredoSolorzano <wsolorzano00@gmail.com>
2026-01-10 21:00:03 -0300

    Fix OrderFactory and ManyToMany handling

--- order/factories.py
+++ order/factories.py
@@ -1,26 +1,11 @@
 import factory
-from factory.django import DjangoModelFactory
-from django.contrib.auth import get_user_model
 from order.models import Order, OrderItem
 from product.factories import ProductFactory
 
-User = get_user_model()
 
-
-class UserFactory(DjangoModelFactory):
-    class Meta:
-        model = User
-
-    username = factory.Faker('user_name')
-    email = factory.Faker('email')
-    password = factory.PostGenerationMethodCall('set_password', 'password123')
-
-
-class OrderFactory(DjangoModelFactory):
+class OrderFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = Order
-
-    user = factory.SubFactory(UserFactory)
 
     @factory.post_generation
     def products(self, create, extracted, **kwargs):
@@ -28,20 +13,9 @@ class OrderFactory(DjangoModelFactory):
             return
 
         if extracted:
-            # Crear OrderItems para cada producto
             for product in extracted:
                 OrderItem.objects.create(
                     order=self,
                     product=product,
-                    quantity=1,
-                    price=product.price
+                    quantity=1
                 )
-        else:
-            # Crear un producto por defecto
-            product = ProductFactory()
-            OrderItem.objects.create(
-                order=self,
-                product=product,
-                quantity=1,
-                price=product.price
-            )
